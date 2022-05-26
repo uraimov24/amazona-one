b397383776bd605a2bbf3c0e14e980df1f6f0250
Niyazbek Uraimov <niyazbek.uraimov.kk@gmail.com>
2022-05-26 19:39:26 +0300

    register

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -4,7 +4,9 @@ import bcrypt from 'bcryptjs';
 import data from '../data.js';
 import User from '../models/userModel.js';
 import { generateToken } from '../utils.js';
+
 const userRouter = express.Router();
+
 userRouter.get(
   '/seed',
   expressAsyncHandler(async (req, res) => {
@@ -13,6 +15,7 @@ userRouter.get(
     res.send({ createdUsers });
   })
 );
+
 userRouter.post(
   '/signin',
   expressAsyncHandler(async (req, res) => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,12 @@ import { signout } from './actions/userActions';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import RegisterScreen from './screens/RegisterScreen';
 import SigninScreen from './screens/SigninScreen';
+
 function App() {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
-
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
   const dispatch = useDispatch();
@@ -54,6 +55,7 @@ function App() {
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All right reserved</footer>
@@ -61,4 +63,5 @@ function App() {
     </BrowserRouter>
   );
 }
+
 export default App;
